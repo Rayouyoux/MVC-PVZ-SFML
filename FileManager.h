089ca57498714b361b738c686202dc870b932f2d
rayan kotob <rayan.kotob22@gmail.com>
2023-11-21 15:41:51 +0100

    Stat reader added

--- FileManager.h
+++ FileManager.h
@@ -16,6 +16,8 @@ public:
 
     std::map<std::string, std::string> getStatsMap(std::vector<std::string> fileLines);
     float getStat(std::map<std::string, std::string> stats, std::string seekedStat);
+
+
 };
 
 #endif /* FILEMANAGER_H */
